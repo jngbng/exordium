ff9d6b797dfeb577869016c6b6f49e716ffa95f8
Jeongbong Seo <jngbng@gmail.com>
2023-01-29 17:19:50 +0900

    local setting

---
+++ local/init-editorconfig.el
@@ -0,0 +1,9 @@
+;; https://github.com/editorconfig/editorconfig-emacs#readme
+
+(use-package editorconfig
+  :ensure t
+  :init
+  (editorconfig-mode 1))
+
+(provide 'init-editorconfig)
+;;; init-editorconfig.el ends here
