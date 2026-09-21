71d425e555b3fccf9573e5839237d3293b19f3a1
Jeongbong Seo <jngbng@gmail.com>
2026-09-21 17:26:35 +0900

    init-cpp의 C-TAB 키바인딩 망가진 것 수정

--- modules/init-cpp.el
+++ modules/init-cpp.el
@@ -195,7 +195,7 @@ With argument, switch to the second choice.  For example, from a
             (t (message "This is not a C/C++ file"))))))
 
 ;;; Ctrl-Tab to switch between .h and .cpp
-(bind-key "C-TAB" #'cpp-switch-h-cpp c-mode-base-map)
+(bind-key "C-<tab>" #'cpp-switch-h-cpp c-mode-base-map)
 
 
 ;;; C++11 keywords
